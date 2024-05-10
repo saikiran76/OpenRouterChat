93cdb1f1c20d029d6b14b1545a6ce5282e58baac
kiran76 <ksknew76@gmail.com>
2024-05-10 23:40:24 +0530

    user slice created

--- src/components/utils/userSlice.js
+++ src/components/utils/userSlice.js
@@ -2,7 +2,18 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
     name: "user",
-    initialState:{
-        
-    }
+    initialState:null,
+    reducers: {
+        addUser: (state, action) => {
+          return action.payload;
+        },
+        removeUser: (state, action) => {
+          return null;
+        },
+      },
+
 })
+
+export const { addUser, removeUser } = userSlice.actions;
+
+export default userSlice.reducer;
