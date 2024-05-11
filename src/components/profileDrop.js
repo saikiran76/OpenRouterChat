0abf36c7e063821e0e2a39818dd4f9ca9c4066d9
kiran76 <ksknew76@gmail.com>
2024-05-11 21:06:50 +0530

    enhacned Recent chat section

--- src/components/profileDrop.js
+++ src/components/profileDrop.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { FaSignOutAlt } from 'react-icons/fa';
+import { FaCircleUser } from "react-icons/fa6";
 import { signOut } from 'firebase/auth';
 import { auth } from './utils/firebase';
 import { removeUser } from './utils/userSlice';
@@ -42,16 +43,17 @@ const ProfileDropdown = () => {
         <FaCaretDown style={{ color: '#fff' }} />
       </button>
       {user && isDropdownOpen && (
-        <div className="absolute right-0 mt-2 w-48 bg-zinc-800 text-white p-4 rounded-md shadow-lg">
-          <p className="text-sm">Signed in as {user.email}</p>
+        <div className="absolute right-0 mt-2 bg-zinc-800 text-white p-4 rounded-md shadow-lg w-[25em]">
+          <p className="text-xs flex items-center gap-2"><FaCircleUser style={{"color":"pink"}}/>{user.email}</p>
           <button onClick={handleLogout} className="block w-full text-left mt-2 text-pink-500">
             <FaSignOutAlt className="mr-2" /> Log Out
           </button>
           <hr className="my-2" />
           <div className="chat-messages max-h-60 overflow-auto">
+            <h1 className='font-bold'>Recent Chat</h1>
             {chats.map((chat, index) => (
               <div key={index} className={`message ${chat.role === 'user' ? 'user-message' : 'assistant-message'}`}>
-                <p className="text-sm p-2 rounded-lg">{chat.content}</p>
+                <p className="text-sm p-2 rounded-lg">{chat.role === 'user' ? (<h1 className='font-semibold text-sm'>User:</h1>) : (<h1 className='font-semibold text-sm'>AI:</h1>)}{chat.content}</p>
               </div>
             ))}
           </div>
